6b458b56a9b8c118c919251f16ea352429d0f692
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-08-04 21:49:39 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
+import Links from "../Links";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
 function App() {
+  const bio = "I am an aspiring software developer from Moringa school";
+  const github = "https://github.com/Feliciakwame";
+  const linkedin = "https://www.linkedin.com/in/felicia-kwame-197ba3285/";
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Felicia" city="Nairobi" />
+      <Links github />
+
+      <About bio={bio} github={github} linkedin={linkedin} isPassed={true} />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href="https://github.com/Feliciakwame">My github</a>
+      <a href="https://www.linkedin.com/in/felicia-kwame-197ba3285/">
+        My linkedIn
+      </a>
+    </div>
+  );
+}
+
+export default Links;
